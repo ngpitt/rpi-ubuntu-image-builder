9057d7b4544abba3220a1faf836b3f090723e8d5
Nick Pitt <ngpitt@xphysics.net>
2017-08-17 13:49:14 -0400

    Updated chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -2,12 +2,19 @@
 
 set -xe
 
-mount --bind /dev rootfs/dev
-mount --bind /proc rootfs/proc
-mount --bind /sys rootfs/sys
+function mount_filesystems {
+    mount --bind /dev rootfs/dev
+    mount --bind /proc rootfs/proc
+    mount --bind /sys rootfs/sys
+}
+
+function unmount_filesystems {
+    umount rootfs/sys
+    umount rootfs/proc
+    umount rootfs/dev
+}
+
+mount_filesystems
+trap unmount_filesystems EXIT
 
 chroot rootfs
-
-umount rootfs/sys
-umount rootfs/proc
-umount rootfs/dev
