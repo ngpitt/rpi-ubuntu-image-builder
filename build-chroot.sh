e200689853aaa01c4cda049bcb657e7c2f4e4f8c
Nick Pitt <ngpitt@xphysics.net>
2017-11-09 11:32:48 -0500

    Switched to the default rasbian kernel

--- build-chroot.sh
+++ build-chroot.sh
@@ -10,15 +10,11 @@ apt install -y \
   apt-transport-https \
   curl
 
-curl https://packagecloud.io/gpg.key | apt-key add -
-echo "deb https://packagecloud.io/Hypriot/rpi/debian/ jessie main
-deb https://packagecloud.io/Hypriot/Schatzkiste/debian/ jessie main" > /etc/apt/sources.list.d/hypriot.list
-
 curl http://archive.raspberrypi.org/debian/raspberrypi.gpg.key | apt-key add -
-echo "deb http://archive.raspberrypi.org/debian/ jessie main" > /etc/apt/sources.list.d/raspberrypi.list
+echo "deb http://archive.raspberrypi.org/debian/ stretch main" > /etc/apt/sources.list.d/raspberrypi.list
 
 curl https://download.docker.com/linux/raspbian/gpg | sudo apt-key add -
-echo "deb [arch=armhf] https://download.docker.com/linux/raspbian jessie stable" > /etc/apt/sources.list.d/docker.list
+echo "deb [arch=armhf] https://download.docker.com/linux/raspbian stretch stable" > /etc/apt/sources.list.d/docker.list
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
@@ -81,8 +77,6 @@ iface eth0 inet dhcp
 
 sed -i "s/%sudo	ALL=(ALL:ALL) ALL/%sudo	ALL=(ALL:ALL) NOPASSWD:ALL/g" /etc/sudoers
 sed -i "s/    HashKnownHosts yes/#   HashKnownHosts yes/g" /etc/ssh/ssh_config
-
-mkinitramfs 4.4.50-hypriotos-v7+ -o /boot/initramfs7.img
 
 rm /etc/update-motd.d/10-help-text
 rm /etc/update-motd.d/50-motd-news
